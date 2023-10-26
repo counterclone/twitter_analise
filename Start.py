efb0c03471d90d2cd695f31881f5fa9d07442761
counterclone <113133440+counterclone@users.noreply.github.com>
2023-10-26 19:00:55 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -9,7 +9,12 @@ import matplotlib.pyplot as plt
 import copy
 import string
 import sys
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
+_ = installff()
 
 st.set_page_config(page_title="Start",page_icon="^0^")
 
