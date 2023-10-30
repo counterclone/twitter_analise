ef7d3de3e177c0d2e56c65a552f2f7773c8b290b
counterclone <113133440+counterclone@users.noreply.github.com>
2023-10-30 15:40:20 +0530

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -52,12 +52,12 @@ class Twitterbot:
         chrome_options = Options()
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         
-        # chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument(f'user-agent={user_agent}')
-        # chrome_options.add_argument("--window-size=1920,1080")
-        # chrome_options.add_argument('--ignore-certificate-errors')
-        # chrome_options.add_argument('--allow-running-insecure-content')
+        chrome_options.add_argument(f'user-agent={user_agent}')
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--allow-running-insecure-content')
         
         self.bot = webdriver.Chrome(options=chrome_options)
         # # chrome_version = "93.0.4577.15"
